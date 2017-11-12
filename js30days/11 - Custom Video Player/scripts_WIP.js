b52b1f79f6329820932150bcb2268540891fd25c
Sotrek <sotrek@gmail.com>
2017-11-12 17:39:15 +0000

    removing empty folder

---
+++ js30days/11 - Custom Video Player/scripts_WIP.js
@@ -0,0 +1,39 @@
+/*  Get our elements   */
+
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const  progress = player.querySelector(".progress");
+const  progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelector("[data-skip]");
+const ranges =  player.querySelector(".player__slider");
+
+/* Build out functions */
+
+function togglePlay() {
+
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    // console.log(icon);
+    toggle.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+/*   Hook up the event listeners   */
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+
